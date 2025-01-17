70cd9d2dc0a25da06ac920ef80f027e68dfd4cc7
viper7lol <chithanh07092004@gmail.com>
2025-01-18 00:47:29 +0700

    tim kiem, xoa

--- ViewModel/CDHCViewModel.cs
+++ ViewModel/CDHCViewModel.cs
@@ -6,6 +6,8 @@ using System.Threading.Tasks;
 using System.Collections.ObjectModel;
 using adc.Model;
 using System.Windows.Input;
+using System.Windows.Controls;
+using System.Windows;
 
 
 namespace adc.ViewModel
@@ -33,6 +35,8 @@ namespace adc.ViewModel
         public string TenCapDo { get => _TenCapDo; set { _TenCapDo = value; OnPropertyChanged(); } }
         public ICommand AddCommand { get; set; }
         public ICommand EditCommand { get; set; }
+        public ICommand DeleteCommand { get; set; }
+        public ICommand SearchCommand { get; set; }
         public CDHCViewModel() {
             CapDoList = new ObservableCollection<CapDoHanhChinh>(DataProvider.Ins.DB.CapDoHanhChinh);
             AddCommand = new RelayCommand<object>((p) =>
@@ -63,6 +67,43 @@ namespace adc.ViewModel
                 var capdohanhchinh = DataProvider.Ins.DB.CapDoHanhChinh.Where(x => x.ID == SelectedCapDo.ID).SingleOrDefault();
                 capdohanhchinh.TenCapDo = TenCapDo;
                 DataProvider.Ins.DB.SaveChanges();
+            });
+            DeleteCommand = new RelayCommand<object>((p) =>
+            {
+                if (string.IsNullOrEmpty(TenCapDo))
+                {
+                    return false;
+                }
+                var capdolist = DataProvider.Ins.DB.CapDoHanhChinh.Where(x => x.TenCapDo == TenCapDo);
+                if (capdolist != null || capdolist.Count() != 0)
+                {
+                    return true;
+                }
+                return true;
+            }, (p) =>
+            {
+                var tencapdo = SelectedCapDo;
+                CapDoList.Remove(SelectedCapDo);
+                DataProvider.Ins.DB.CapDoHanhChinh.Remove(tencapdo);
+                DataProvider.Ins.DB.SaveChanges();
+            });
+            SearchCommand = new RelayCommand<object>((p) =>
+            {
+                return true;
+            }, (p) =>
+            {
+                string searchName = TenCapDo;
+                if(searchName != null)
+                {
+                    foreach(var item in CapDoList)
+                    {
+                        if(item.TenCapDo.Contains(searchName)) SelectedCapDo = item;
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("Không tìm thấy kết quả nào.", "Thông báo", MessageBoxButton.OK, MessageBoxImage.Information);
+                }
             });
         }
     }
