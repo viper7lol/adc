73fdb9622a9ac7b10c223d22f16af1b46ee1a614
viper7lol <chithanh07092004@gmail.com>
2025-02-05 04:27:14 +0700

    sdsds

--- ViewModel/LSTCViewModel.cs
+++ ViewModel/LSTCViewModel.cs
@@ -10,10 +10,48 @@ namespace adc.ViewModel
 {
     public class LSTCViewModel:BaseViewModel
     {
+        private string _MoTaHanhDong;
+        public string MoTaHanhDong { get => _MoTaHanhDong; set { _MoTaHanhDong = value; OnPropertyChanged(); } }
+        private int _ID;
+        public int ID { get => _ID; set {  _ID = value; OnPropertyChanged(); } }
+        private int? _UserID;
+        public int? UserID { get => _UserID; set {  _UserID = value; OnPropertyChanged(); } }
+        private DateTime? _ThoiGianTruyCap;
+        public DateTime? ThoiGianTruyCap { get => _ThoiGianTruyCap; set { _ThoiGianTruyCap = value; OnPropertyChanged(); } }
         private ObservableCollection<LichSuTruyCap> _LSTCList;
         public ObservableCollection<LichSuTruyCap> LSTCList { get => _LSTCList; set { _LSTCList = value; OnPropertyChanged(); } }
         public LSTCViewModel()
         {
+            MoTaHanhDong = "";
+            LoginWindow lg = new LoginWindow();
+            var loginVM = lg.DataContext as LoginViewModel;
+            if (loginVM.IsLogin)
+            {
+                if (loginVM.IsAdmin)
+                {
+                    var lichSu = new LichSuTruyCap
+                    {
+                        UserID = 1,
+                        ThoiGianTruyCap = DateTime.Now,
+                        MoTaHanhDong = "đã đăng nhập vào hệ thống"
+                    };
+                    DataProvider.Ins.DB.LichSuTruyCap.Add(lichSu);
+                    DataProvider.Ins.DB.SaveChanges();
+                    LSTCList.Add(lichSu);
+                }
+                else
+                {
+                    var lichSu = new LichSuTruyCap
+                    {
+                        UserID = 2,
+                        ThoiGianTruyCap = DateTime.Now,
+                        MoTaHanhDong = "đã đăng nhập vào hệ thống"
+                    };
+                    DataProvider.Ins.DB.LichSuTruyCap.Add(lichSu);
+                    DataProvider.Ins.DB.SaveChanges();
+                    LSTCList.Add(lichSu);
+                }
+            }
             LSTCList = new ObservableCollection<LichSuTruyCap>(DataProvider.Ins.DB.LichSuTruyCap);
         }
         public void GhiLichSu(int userId, string moTaHanhDong)
