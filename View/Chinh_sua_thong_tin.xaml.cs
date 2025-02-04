77678170b113f6c2c3c092b130573141f832b187
Tran Sang <Sang.TL223808@sis.hust.edu.vn>
2025-02-04 17:06:36 +0700

    OK

---
+++ View/Chinh_sua_thong_tin.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace adc.View
+{
+	/// <summary>
+	/// Interaction logic for Chinh_sua_thong_tin.xaml
+	/// </summary>
+	public partial class Chinh_sua_thong_tin : Window
+	{
+		public Chinh_sua_thong_tin()
+		{
+			InitializeComponent();
+		}
+	}
+}
